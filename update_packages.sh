fc51bb96c5c7f0c6e2236a45ceb67b7d7e954527
Tim Whiting <tim@whitings.org>
2020-11-06 13:19:06 -0700

    updates

---
+++ update_packages.sh
@@ -0,0 +1,5 @@
+# Just the ones that are needed for dartros to work
+cp -r devel/share/gendart/ros/actionlib_msgs src/
+cp -r devel/share/gendart/ros/std_msgs src/
+cp -r devel/share/gendart/ros/rosgraph_msgs src/
+cp -r devel/share/gendart/ros/roscpp src/
