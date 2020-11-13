f75477afb15a42faf244044ef44975912f82e7b8
Tim Whiting <tim@whitings.org>
2020-11-13 09:24:00 -0700

    update packages

--- update_packages.sh
+++ update_packages.sh
@@ -3,3 +3,5 @@ cp -r devel/share/gendart/ros/actionlib_msgs src/
 cp -r devel/share/gendart/ros/std_msgs src/
 cp -r devel/share/gendart/ros/rosgraph_msgs src/
 cp -r devel/share/gendart/ros/roscpp src/
+cp -r devel/share/gendart/ros/sensor_msgs src/
+cp -r devel/share/gendart/ros/geometry_msgs src/
